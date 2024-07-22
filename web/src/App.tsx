d2647a5f20710de77bdc2cb3ca4fc98bf200e311
Isaac S. Silva <85599926+IsaacGSS@users.noreply.github.com>
2024-07-22 14:03:47 +0000

    init front end web

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Short Links</h1>
+        <input type="text" placeholder='http://seulinkaqui.com/...' />
+      </main>
+    </>
+  )
+}
+
+export default App
